c667232f42835e3e683f87ace42f94eaef0ec625
walldba <wallacedba@gmail.com>
2025-09-05 10:17:16 -0300

    feat: initialize api with basic configs

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { abortOnError: true }
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Scrooge Bank API')
+    .setDescription('Scrooge Bank API - SFox Code Challenge')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+}
+bootstrap();
