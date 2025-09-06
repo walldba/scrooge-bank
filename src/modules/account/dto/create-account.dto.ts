42ebe6d78e5ff8d55db84c066f7eb9daf69a61c6
walldba <wallacedba@gmail.com>
2025-09-05 21:30:58 -0300

    feat: add swagger decorators on dtos

--- src/modules/account/dto/create-account.dto.ts
+++ src/modules/account/dto/create-account.dto.ts
@@ -1,12 +1,20 @@
 import { IsOptional, IsString, ValidateNested } from 'class-validator';
 import { CreateUserDto } from '../../user/dto/create-user.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAccountDto {
+  @ApiProperty({
+    description: 'Account number',
+    example: '1234567890',
+  })
   @IsString()
-  @IsOptional()
   accountNumber: string;
 
+  @ApiProperty({
+    description: 'User details associated with the account',
+    type: () => CreateUserDto,
+  })
   @ValidateNested()
   @Type(() => CreateUserDto)
   user: CreateUserDto;
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'First name of the user',
+    example: 'John',
+  })
   @IsString()
   firstName: string;
 
+  @ApiProperty({
+    description: 'Last name of the user',
+    example: 'Doe',
+  })
   @IsString()
   lastName: string;
 
+  @ApiProperty({
+    description: 'Email address of the user',
+    example: 'john.doe@example.com',
+  })
   @IsEmail()
   mail: string;
 }
